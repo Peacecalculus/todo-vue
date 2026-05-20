4a5f0c5734eed5d1d3b9b032ae4255ed58b32141
Peacecalculus <Peaceezekiel24@gmail.com>
2026-05-20 19:50:03 +0100

    feat(todo): add real-time task updates via socket

--- src/components/ErrorTest.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function ErrorTest(){
-  // Throw when this component is rendered to exercise ErrorBoundary
-  throw new Error('Test error for ErrorBoundary')
-}
